5ada7733bbb31c3748973a2ef970df8d048c1f03
Ian Lesperance <ian@elliterate.com>
2016-08-01 18:56:37 -0400

    Add tests for 0 and 2+ descendants

--- tests/dsl/test_descendant.py
+++ tests/dsl/test_descendant.py
@@ -17,3 +17,15 @@ class TestDescendant(DSLTestCase):
         xpath = to_xpath(descendant("p")[attr("id").equals(foo_div.attr("title"))])
         results = self.find_all(xpath)
         self.assertSequenceEqual(results, [])
+
+    def test_finds_multiple_kinds_of_nodes(self):
+        xpath = to_xpath(descendant("p", "ul"))
+        results = self.find_all(xpath)
+        self.assertEqual(results[0].text, "Blah")
+        self.assertEqual(results[3].text, "A list")
+
+    def test_finds_all_nodes_when_no_arguments_given(self):
+        xpath = to_xpath(descendant()[attr("id").equals("foo")].descendant())
+        results = self.find_all(xpath)
+        self.assertEqual(results[0].text, "Blah")
+        self.assertEqual(results[4].text, "A list")
